98f5c6dc6586295594cb32da2b8bab4efdf6a3c8
Ever Cifuentes <everbilly35@gmail.com>
2020-09-13 21:02:05 -0600

    02 Methods, V-Model, and Computed with VueJS

--- index.js
+++ index.js
@@ -7,14 +7,26 @@ var app = new Vue({
             { nombre: 'Manzana', cantidad: 2 },
             { nombre: 'Durazno', cantidad: 0 }
         ],
-        nuevaFruta: ''
+        nuevaFruta: '',
+        total: 0
     },
     methods: {
         agregarFruta(){
             // console.log('Click')
             this.frutas.push({
-                 nombre: this.nuevaFruta, cantidad: 0
+                 nombre: this.nuevaFruta,
+                 cantidad: this.cantidad
             })
+            this.nuevaFruta = ''
+        }
+    },
+    computed: {
+        sumarFrutas(){
+            this.total = 0;
+            for(fruta of this.frutas){
+                this.total = this.total + fruta.cantidad
+            }
+            return this.total
         }
     }
 })
